8fc38c9531bb3056ca405a56c6868ae1ba8e8676
jzaldivar <jorge.zaldivar@gmail.com>
2026-09-17 20:12:15 -0600

    Revert "Remove solution!"
    This reverts commit ad091e63a84798234f9253df064a02f3ba95a744.

--- segundo_siguiente.py
+++ segundo_siguiente.py
@@ -1,15 +1,23 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Calcular la hora correspondiente al siguiente segundo
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+horas = int(input("Hora: "))
+minutos = int(input("Minutos: "))
+segundos = int(input("Segundos: "))
 
 # Proceso
-salida = entrada
+segundos += 1
+if segundos > 59:
+    segundos = 0
+    minutos += 1
+    if minutos > 59:
+        minutos = 0
+        horas += 1
+        if horas > 23:
+            horas = 0
+tiempo = f'{horas:02d}:{minutos:02d}:{segundos:02d}'
 
 # Salidas
-print(salida)
+print('Hora al siguiente segundo >', tiempo)
